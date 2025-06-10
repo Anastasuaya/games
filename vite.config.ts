934b667c34b65a605aa08c52321da59221cb5fda
Anastasuaya <anastasuaya.bashlykova09@gmail.com>
2025-06-10 12:34:01 +0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,17 @@
 export default {
-    base: '/games/'
+    base: '/games/',
+    build: {
+        rollupOptions: {
+            input: {
+                // @ts-ignore
+                main: resolve(__dirname, 'index.html'),
+                // @ts-ignore
+                gallows: resolve(__dirname, 'gallows.html'),
+                // @ts-ignore        
+                memgen: resolve(__dirname, 'memGen.html'),
+                // @ts-ignore        
+                matrix: resolve(__dirname, 'matrix.html'),
+            }
+        }
+    }
 }
